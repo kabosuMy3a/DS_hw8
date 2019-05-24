1937e194bfa1d1ed874977263afed2d0f572e8e0
kabosuMy3a <my3a3380@gmail.com>
2019-05-24 21:12:42 +0900

    sundone

--- main.cpp
+++ main.cpp
@@ -33,41 +33,48 @@ int main() {
 
 		key = trim(temp1);
 		value = trim(temp2);
-		cout <<key << "| " << value<<endl;
+
 		bstTree->insert(key,value);
-		
-		//bstTree->inOrderPrint();
+
 
 	}
-
-	bstTree->inOrderPrint();
 
 	string file_line;
 	while(!infile.eof()){
 
-		char temp = '\0' ;	
+		char temp = '\0';
 		infile >> file_line;
-
-		temp = file_line.at(file_line.size()-1);
-		bool flag = false ;
-		if (temp ==',' || temp=='!' || temp=='?' || temp=='.'){
-			flag = true;
-			file_line = file_line.substr(0,file_line.size()-1);
-		}
-			
-		string target = bstTree->getData(file_line);
-		if (!target.empty()) file_line= target ;
 		
-
-		if (flag == true) cout << file_line << temp << " ";
-		else cout << file_line << " " ;
-
+		string file_line2 = file_line;
+			
+		temp = file_line.at(file_line.size()-1);
+		bool flag = false;
+		
+		if (temp==','|| temp=='!' || temp=='?'|| temp=='.' || temp==';'){
+		    flag = true;
+		    file_line = file_line.substr(0,file_line.size()-1);
+		}
+		    
+		string target = bstTree->getData(file_line);
+		string target2 = bstTree->getData(file_line2);
+		
+		
+		if (!target.empty()) file_line= target ;
+        if (!target2.empty()){
+            flag = false ;
+            file_line = target2;
+        }
+        
+        if (flag==true) cout << file_line << temp ;
+        else cout << file_line ;
+        
+        if(infile.peek()==' '){
+            cout << " " ;
+        
+        }
 		if(infile.get() == '\n'){
 			cout << endl;
 		}
-
-
-		
 		
 	}
 	
@@ -109,5 +116,6 @@ char * trim(char * s){
 	return s ;
 
 }
+
 
 
